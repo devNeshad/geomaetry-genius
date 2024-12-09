6cef0145f84282a978bff534da9fa4e6b4a7f9f9
devNeshad <mdneashads@gmail.com>
2024-12-10 00:37:08 +0600

    setup

--- script/rectangle-area.js
+++ script/rectangle-area.js
@@ -1,4 +1,21 @@
 function calculateRectangleArea() {
-    console.log('calculete ract area');
-    
+  // grt length of the rectangle
+  const lengthInput = document.getElementById('rectangle-length')
+  const lengthText = lengthInput.value
+  const length = parseFloat(lengthText)
+  console.log(length)
+
+  // grt width of the rectangle
+  const widthInput = document.getElementById('rectangle-width')
+  const widthText = widthInput.value
+  const width = parseFloat(widthText)
+  console.log(width)
+
+  // calculate rectangel area
+  const area = length * width
+  console.log('area of the rectangle: ', area)
+
+  // display rectangle area
+  const rectangleAreaSpan = document.getElementById('rectangle-area')
+  rectangleAreaSpan.innerText = area
 }
